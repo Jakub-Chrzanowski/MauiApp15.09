eed76e6b0fc13f6cec17573114c563684e7ecd3f
Jakub Chrzanowski <kubek1290pl@gmail.com>
2025-09-17 12:03:09 +0200

    Release 1.0

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -14,7 +14,29 @@ namespace MauiApp2
 
         private void Add_Product_Clicked(object sender, EventArgs e)
         {
-            Products.Add(new Item { Name = Name.Text, Quantity = int.Parse(Quantity.Text) });
+            int QuantityParsed;
+            
+            if (int.TryParse(Quantity.Text, out QuantityParsed))
+            {
+                Products.Add(new Item { Name = Name.Text, Quantity = QuantityParsed});
+            }
+            else
+            {
+                DisplayAlert("Błąd", "Podaj poprawną ilość", "OK");
+            }
+        }
+
+        private void Delete_Product_Clicked(object sender, EventArgs e)
+        {
+            var selectedItem = Products_Collection_View.SelectedItem as Item;
+            if (selectedItem !=  null)
+            {
+                Products.Remove(selectedItem);
+            }
+            else
+            {
+                DisplayAlert("Info", "Nie wybrano żadnego elementu", "OK");
+            }
         }
     }
 }
